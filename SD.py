b5a2279a2a21fe3a22579e1e1ace8a544bf29d49
ShubhangiDhurve <84616301+ShubhangiDhurve@users.noreply.github.com>
2021-07-17 12:26:11 +0530

    Add files via upload

---
+++ SD.py
@@ -0,0 +1,49 @@
+# a=[11,23,30,45,10,8,9]
+# b=[]
+# sum=0
+# count=0
+# sum1=0
+# count1=0
+# c=[]
+# i=0
+# while i<len(a):
+#         if a[i]%2==0:
+#             sum=sum+a[i]
+#             count+=1
+#         # else:
+#         #     sum1=sum1+a[i]
+#         #     count1+=1
+#         b.append()
+#         c.append()
+#         i=i+1
+# print(b)
+# print(c)
+a=[11,23,30,45,10,8,9]
+b=[]
+sum=0
+count=0
+# sum1=0
+# count1=0
+c=[]
+sum1=0
+count1=0
+i=0
+while i<len(a):
+        if a[i]%2==0:
+#             sum=sum+a[i]
+#             count+=1
+#             b.append(a[i])
+#         else:
+#             sum1=sum1+a[i]
+#             count1=count1+1
+#             c.append(a[i])
+#         # b.append(a[i])
+#             # c.append(a[i])
+#         i=i+1
+# print(b,sum,count)
+# print(c,sum1,count1)
+# print(coun
+      
+
+
+
